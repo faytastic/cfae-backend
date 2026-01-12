ccc7219691261fa3d44842fa7dd6c4c83ce8a40a
faytastic <lichter.faigy@gmail.com>
2026-01-12 16:44:57 -0500

    Add separate config for dev and prod environments

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
 
-class Config:
-    DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+class BaseConfig:
+    DEBUG = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
     SECRET_KEY = os.getenv("SECRET_KEY", "development-key")
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
