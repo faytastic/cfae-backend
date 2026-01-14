696df2cb8ef419c4b31a399bc7de7a3a8c0b78d8
faytastic <lichter.faigy@gmail.com>
2026-01-14 16:43:51 -0500

    Register admin routes blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,9 @@ def create_app():
     from .routes import register_routes
     register_routes(app)
 
+    # Register admin routes
+    from .admin_routes import admin_bp
+    app.register_blueprint(admin_bp)
+
     return app
+
