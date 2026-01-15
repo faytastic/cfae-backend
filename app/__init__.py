dc2c6d9767735040a0027f192fbaae2895891acc
faytastic <lichter.faigy@gmail.com>
2026-01-14 22:36:41 -0500

    Fix template path

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates")
+
 
     # Import routes after app is created
     from .routes import register_routes
